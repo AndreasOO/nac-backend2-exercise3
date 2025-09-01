a9f49bf68c39ea36e49a4bf4c81c1cf8895628d1
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-09-01 09:25:51 +0200

    initial commit

---
+++ src/main/java/org/springframework/backend2exercise3/Backend2Exercise3Application.java
@@ -0,0 +1,13 @@
+package org.springframework.backend2exercise3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend2Exercise3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend2Exercise3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/springframework/backend2exercise3/Backend2Exercise3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.backend2exercise3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend2Exercise3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
